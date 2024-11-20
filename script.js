70bba67037a942149b965b0c70b18c43ecba2c10
Soham Khade <sohambkhade@gmail.com>
2024-11-19 20:25:44 -0800

    navbar

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// Overlay
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+}
+// Front Page typed JS
 var options = {
     strings: ["Software Engineer", "Web Developer"],
     typeSpeed: 100,  // speed of typing
@@ -7,3 +16,6 @@ var options = {
 };
 
 var typed = new Typed(".typed", options);
+
+
+
